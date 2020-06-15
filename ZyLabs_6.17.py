9d76bc8cd95d0a23b3e6a1706d8c8ba1608f0338
Ken Valverde <verify>
2020-06-14 20:37:09 -0500

    final ver

--- ZyLabs_6.17.py
+++ ZyLabs_6.17.py
@@ -2,19 +2,18 @@ txt = input('Create password: ')
 password = ''
 
 for x in txt:
-    if x == 'i':
-        password +='!'
-    elif x == 'a':
+    if (x=='i'):
+        password += '!'
+    elif (x=='a'):
         password += '@'
-    elif x == 'm':
+    elif (x=='m'):
         password += 'M'
-    elif x == 'B':
-        password += '8'
-    elif x == 'o':
-        password = '.'
+    elif (x=='B'):
+        password +='8'
+    elif (x=='o'):
+        password += '.'
     else:
         password += x
+password += 'q*s'
 
-
-print(password)
-
+print('Your better password is:', password)
