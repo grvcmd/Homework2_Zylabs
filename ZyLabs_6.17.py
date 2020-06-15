dba2a54772f40e64144f877760ce1aa2ce64f963
Ken Valverde <verify>
2020-06-14 20:26:16 -0500

    ver. 1

---
+++ ZyLabs_6.17.py
@@ -0,0 +1,20 @@
+txt = input('Create password: ')
+password = ''
+
+for x in txt:
+    if x == 'i':
+        password +='!'
+    elif x == 'a':
+        password += '@'
+    elif x == 'm':
+        password += 'M'
+    elif x == 'B':
+        password += '8'
+    elif x == 'o':
+        password = '.'
+    else:
+        password += x
+
+
+print(password)
+
